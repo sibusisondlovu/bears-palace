afeb083abd80e50846426f963274e37e23d48193
Sibusiso Ndlovu <sibusiso@jaspa.co.za>
2021-06-03 15:56:14 +0200

    resort screen

---
+++ lib/screens/resort_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResortScreen extends StatefulWidget {
+  const ResortScreen({Key key}) : super(key: key);
+
+  @override
+  _ResortScreenState createState() => _ResortScreenState();
+}
+
+class _ResortScreenState extends State<ResortScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
